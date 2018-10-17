6ae04d3f3eed8b70023aa11ec187c3848b17859b
Pierre Bolot <pbolot@b-p.fr>
2018-10-17 14:00:46 +0200

    test

--- MyExtensions/AddIns.extension/AddIns.tab/Coupes.panel/MWS.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/Coupes.panel/MWS.pushbutton/script.py
@@ -19,11 +19,11 @@ options = __revit__.Application.Create.NewGeometryOptions()
 
 def get_selected_elements(doc):
     try:
-        # Revit 2016
+        # # Revit 2016
         return [doc.GetElement(id)
                 for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
     except:
-        # old method
+        # # old method
         return list(__revit__.ActiveUIDocument.Selection.Elements)
 
 try:
