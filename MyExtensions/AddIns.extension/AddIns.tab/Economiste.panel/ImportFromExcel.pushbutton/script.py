137407e426b5a18759773e083cbee5e2bdd7f66e
Cleuteu <>
2018-10-18 18:24:00 +0200

    modification of import excel, can now choose the worksheet by name and row colone numbers

--- MyExtensions/AddIns.extension/AddIns.tab/Economiste.panel/ImportFromExcel.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/Economiste.panel/ImportFromExcel.pushbutton/script.py
@@ -7,6 +7,8 @@ from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
 from System.Collections.Generic import List
 from pyrevit import forms
+from rpw.ui.forms import TextInput
+
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
@@ -14,119 +16,128 @@ options = __revit__.Application.Create.NewGeometryOptions()
 SEBoptions = SpatialElementBoundaryOptions()
 roomcalculator = SpatialElementGeometryCalculator(doc)
 
-td_button = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.Cancel | TaskDialogCommonButtons.No
+td_button = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No
 
-res = TaskDialog.Show("Importation from Excel","Le fichier Excel est-il ouvert sur le bon onglet? Les ids des elements doivent etre en colonne 1 et les noms de parametres partages en ligne 1",td_button)
+res = TaskDialog.Show("Importation from Excel","Attention :\n- Les ids des elements doivent etre en colonne 1\n- Les noms exacts (avec majuscules) des parametres partages doivent etre en ligne 1\n- Aucun accent ou caractere special dans le fichier Excel", td_button)
 
 if res == TaskDialogResult.Yes:
                
                # Ungroup function 
-               def Ungroup(group):
-                              group.UngroupMembers()
-                              
-               # Regroup function 
-               def Regroup(groupname,groupmember):
-                              newgroup = doc.Create.NewGroup(groupmember)
-                              newgroup.GroupType.Name = str(groupname)
-               
-               def convertStr(s):
-                   """Convert string to either int or float."""
-                   try:
-                       ret = int(s)
-                   except ValueError:
-                       ret = 0
-                   return ret
-                              
-               #Open a form and choose the worksheet number
-               ops = ['1', '2', '3', '4','5']
-               worksheet = forms.CommandSwitchWindow.show(ops, message='Select the number of the worksheet in Excel')
-               worksheet = convertStr(worksheet)
+    def Ungroup(group):
+                  group.UngroupMembers()
+                  
+   # Regroup function 
+    def Regroup(groupname,groupmember):
+                  newgroup = doc.Create.NewGroup(groupmember)
+                  newgroup.GroupType.Name = str(groupname)
+   
+    def convertStr(s):
+       """Convert string to either int or float."""
+       try:
+           ret = int(s)
+       except ValueError:
+           ret = 0
+       return ret
+                  
+   #Open a form and choose the worksheet number
+    worksheetInput = TextInput('Name of the sheet to import', default = "Door sheet")
+    rowEnd = convertStr(TextInput('Numero de la derniere ligne a importer', default = "45"))
+    colEnd = convertStr(TextInput('Numero de la derniere colonne a importer', default = "45"))
 
-               t = Transaction(doc, 'Read Excel spreadsheet.') 
-               t.Start()
-               
-               #Accessing the Excel applications.
-               xlApp = System.Runtime.InteropServices.Marshal.GetActiveObject('Excel.Application')
-               
-               #Worksheet, Row, and Column parameters
-               # worksheet = 2
-               rowStart = 1
-               rowEnd = 900
-               column_id = 1
-               colStart = 2
-               colEnd = 3
-               
-               #Using a loop to read a range of values and print them to the console.
-               array = []
-               param_names_excel = []
-               data = {}
-               for r in range(rowStart, rowEnd):
-                              data_id = xlApp.Worksheets(worksheet).Cells(r, column_id).Text
-                              data_id_int = convertStr(data_id)
-                              if data_id_int != 0:
-                                            data = {'id': data_id_int}
-                                            for c in range(colStart, colEnd):
-                                                           data_param_value = xlApp.Worksheets(worksheet).Cells(r, c).Text
-                                                           data_param_name = xlApp.Worksheets(worksheet).Cells(1, c).Text
-                                                           if data_param_name != '':
-                                                                          param_names_excel.append(data_param_name)
-                                                                          if data_param_value != '':
-                                                                                         data[data_param_name] = data_param_value
-                                            array.append(data)
+    t = Transaction(doc, 'Read Excel spreadsheet.') 
+    t.Start()
+   
+    #Accessing the Excel applications.
+    xlApp = System.Runtime.InteropServices.Marshal.GetActiveObject('Excel.Application')
+    count = 1
+    worksheet = 0
+    for i in xlApp.Worksheets:
+      if i.Name == worksheetInput:
+      worksheet = i
+    # count += 1
+
+     # if worksheet == 0:
+          # TaskDialog.Show("Ce nom d'onglet Excel n\'existe pas dans votre fichier.\n Try again!", TaskDialogCommonButtons.Yes)
+ 
+    #Row, and Column parameters
+    rowStart = 1
+    # rowEnd = 35
+    column_id = 1
+    colStart = 2
+    # colEnd = 3
+
+
+    # Using a loop to read a range of values and print them to the console.
+    array = []
+    param_names_excel = []
+    data = {}
+    for r in range(rowStart, rowEnd):
+        data_id = worksheet.Cells(r, column_id).Text
+        data_id_int = convertStr(data_id)
+        if data_id_int != 0:
+            data = {'id': data_id_int}
+            for c in range(colStart, colEnd):
+                data_param_value = worksheet.Cells(r, c).Text
+                data_param_name = worksheet.Cells(1, c).Text
+                if data_param_name != '':
+                    param_names_excel.append(data_param_name)
+                    if data_param_value != '':
+                        data[data_param_name] = data_param_value
+            array.append(data)
                
                               
-               t.Commit()
+    t.Commit()
                
-               #Recuperation des portes
-               doors = FilteredElementCollector(doc)\
-                                              .OfCategory(BuiltInCategory.OST_Doors)\
-                                              .WhereElementIsNotElementType()\
-                                              .ToElements()
-               
-               #Get parameters in the model
-               params_door_set = doors[0].Parameters
-               params_door_name = []
-               for param_door in params_door_set:
-                              params_door_name.append(param_door.Definition.Name)
-                              
-               unfounddoors = []
-               
-               t = Transaction(doc, 'Feed doors')
-               t.Start()
+    #Recuperation des portes
+    doors = FilteredElementCollector(doc)\
+          .OfCategory(BuiltInCategory.OST_Doors)\
+          .WhereElementIsNotElementType()\
+          .ToElements()
+   
+    #Get parameters in the model
+    params_door_set = doors[0].Parameters
+    params_door_name = []
+    for param_door in params_door_set:
+     params_door_name.append(param_door.Definition.Name)
+                  
+    unfounddoors = []
+   
+    t = Transaction(doc, 'Feed doors')
+    t.Start()
 
-               for hash in array:
-                              idInt = int(hash['id'])
-                              try :
-                                            door_id = ElementId(idInt)
-                                            door = doc.GetElement(door_id)
-                                            groupId = door.GroupId
-                                            if str(groupId) != "-1":
-                                                           group = doc.GetElement(groupId)
-                                                           groupname = group.Name
-                                                           groupmember = group.GetMemberIds()
-                                                           Ungroup(group)
-                                                           
-                                            for param in param_names_excel:
-                                                           if (param in params_door_name) and (param in hash):
-                                                                          door.LookupParameter(param).Set(hash[param])
-                                                                          
-                                            if str(groupId) != "-1":
-                                                           Regroup(groupname,groupmember)
-                                                           # if "(membre exclu)" in group.GroupType.Name:
-                                                                          # group.GroupType.Name = groupname
-                                            print("Door " + str(idInt) + " : OK")
+    for hash in array:
+        idInt = int(hash['id'])
+        try :
+            door_id = ElementId(idInt)
+            door = doc.GetElement(door_id)
+            groupId = door.GroupId
+            if str(groupId) != "-1":
+                group = doc.GetElement(groupId)
+                groupname = group.Name
+                groupmember = group.GetMemberIds()
+                Ungroup(group)
+                           
+            for param in param_names_excel:
+                if (param in params_door_name) and (param in hash):
+                    door.LookupParameter(param).Set(hash[param])
+                                          
+            if str(groupId) != "-1":
+                Regroup(groupname,groupmember)
+                    # if "(membre exclu)" in group.GroupType.Name:
+                    # group.GroupType.Name = groupname
+            print("Door " + str(idInt) + " : OK")
 
-                              except:
-                                            print(str(idInt) + " not in REVIT doc")
-                                            unfounddoors.append(idInt)
-                                            
-               print("Job done!")
-               
-               t.Commit()
+        except:
+            print(str(idInt) + " not in REVIT doc")
+            unfounddoors.append(idInt)
+                                
+    print("Job done!")
+   
+    t.Commit()
 
-               if len(unfounddoors) != 0:
-                              print(str(len(unfounddoors)) + " doors not found : ")
-                              print(unfounddoors)
+    if len(unfounddoors) != 0:
+        print(str(len(unfounddoors)) + " doors not found : ")
+        print(unfounddoors)
 
 else:
-               "A plus tard!"
+    "A plus tard!"
--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestAlex.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestAlex.pushbutton/script.py
@@ -1,60 +1,143 @@
-"""Remplit les IDs des portes"""
-
-__title__ = 'Set door\n IDs'
-
-__doc__ = 'Ce programme remplit les IDs des portes'
-
 import clr
+import System
 import math
-clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI') 
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
 from System.Collections.Generic import List
-from rpw.ui.forms import TextInput
 from pyrevit import forms
+from rpw.ui.forms import TextInput
 
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 options = __revit__.Application.Create.NewGeometryOptions()
 SEBoptions = SpatialElementBoundaryOptions()
+roomcalculator = SpatialElementGeometryCalculator(doc)
 
-from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox, TextBox,
-		                  Separator, Button)
-components = [Label('Pick a category:'),
-              ComboBox('combobox1', {'Doors': 0, 'Rooms': 1}),
-              Label('Enter the name of ID parameter:'),
-              TextBox('textbox1', Text="ID Revit"),
-              Separator(),
-              Button('Select')]
-form = FlexForm('Title', components)
-form.show()
-# User selects `Opt 1`, types 'Wood' in TextBox, and select Checkbox
-print form.values
+td_button = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No
 
-parameterName = form.values['combobox1']
-category = form.values['combobox1']
+res = TaskDialog.Show("Importation from Excel","Attention :\n- Les ids des elements doivent etre en colonne 1\n- Les noms exacts (avec majuscules) des parametres partages doivent etre en ligne 1\n- Aucun accent ou caractere special dans le fichier Excel", td_button)
 
-if category == 0:
-	collector = FilteredElementCollector(doc)\
-		 		.OfCategory(BuiltInCategory.OST_Doors)\
-		  		.WhereElementIsNotElementType()\
-		  		.ToElements()
-	print 'ici'
+if res == TaskDialogResult.Yes:
+               
+               # Ungroup function 
+    def Ungroup(group):
+                  group.UngroupMembers()
+                  
+   # Regroup function 
+    def Regroup(groupname,groupmember):
+                  newgroup = doc.Create.NewGroup(groupmember)
+                  newgroup.GroupType.Name = str(groupname)
+   
+    def convertStr(s):
+       """Convert string to either int or float."""
+       try:
+           ret = int(s)
+       except ValueError:
+           ret = 0
+       return ret
+                  
+   #Open a form and choose the worksheet number
+    worksheetInput = TextInput('Name of the sheet to import', default = "Door sheet")
+    rowEnd = convertStr(TextInput('Numero de la derniere ligne a importer', default = "45"))
+    colEnd = convertStr(TextInput('Numero de la derniere colonne a importer', default = "45"))
+
+    t = Transaction(doc, 'Read Excel spreadsheet.') 
+    t.Start()
+   
+    #Accessing the Excel applications.
+    xlApp = System.Runtime.InteropServices.Marshal.GetActiveObject('Excel.Application')
+    count = 1
+    worksheet = 0
+    for i in xlApp.Worksheets:
+	    if i.Name == worksheetInput:
+			worksheet = i
+		# count += 1
+
+     # if worksheet == 0:
+					# TaskDialog.Show("Ce nom d'onglet Excel n\'existe pas dans votre fichier.\n Try again!", TaskDialogCommonButtons.Yes)
+ 
+    #Row, and Column parameters
+    rowStart = 1
+    # rowEnd = 35
+    column_id = 1
+    colStart = 2
+    # colEnd = 3
+
+
+    # Using a loop to read a range of values and print them to the console.
+    array = []
+    param_names_excel = []
+    data = {}
+    for r in range(rowStart, rowEnd):
+        data_id = worksheet.Cells(r, column_id).Text
+        data_id_int = convertStr(data_id)
+        if data_id_int != 0:
+            data = {'id': data_id_int}
+            for c in range(colStart, colEnd):
+                data_param_value = worksheet.Cells(r, c).Text
+                data_param_name = worksheet.Cells(1, c).Text
+                if data_param_name != '':
+                    param_names_excel.append(data_param_name)
+                    if data_param_value != '':
+                        data[data_param_name] = data_param_value
+            array.append(data)
+               
+                              
+    t.Commit()
+               
+    #Recuperation des portes
+    doors = FilteredElementCollector(doc)\
+          .OfCategory(BuiltInCategory.OST_Doors)\
+          .WhereElementIsNotElementType()\
+          .ToElements()
+   
+    #Get parameters in the model
+    params_door_set = doors[0].Parameters
+    params_door_name = []
+    for param_door in params_door_set:
+ 	   params_door_name.append(param_door.Definition.Name)
+                  
+    unfounddoors = []
+   
+    t = Transaction(doc, 'Feed doors')
+    t.Start()
+
+    for hash in array:
+        idInt = int(hash['id'])
+        try :
+            door_id = ElementId(idInt)
+            door = doc.GetElement(door_id)
+            groupId = door.GroupId
+            if str(groupId) != "-1":
+                group = doc.GetElement(groupId)
+                groupname = group.Name
+                groupmember = group.GetMemberIds()
+                Ungroup(group)
+                           
+            for param in param_names_excel:
+                if (param in params_door_name) and (param in hash):
+                    door.LookupParameter(param).Set(hash[param])
+                                          
+            if str(groupId) != "-1":
+                Regroup(groupname,groupmember)
+                    # if "(membre exclu)" in group.GroupType.Name:
+                    # group.GroupType.Name = groupname
+            print("Door " + str(idInt) + " : OK")
+
+        except:
+            print(str(idInt) + " not in REVIT doc")
+            unfounddoors.append(idInt)
+                                
+    print("Job done!")
+   
+    t.Commit()
+
+    if len(unfounddoors) != 0:
+        print(str(len(unfounddoors)) + " doors not found : ")
+        print(unfounddoors)
+
 else:
-	collector = FilteredElementCollector(doc)\
-		 		.OfCategory(BuiltInCategory.OST_Rooms)\
-		  		.WhereElementIsNotElementType()\
-		  		.ToElements()
-	print 'la!'
-
-t = Transaction(doc, 'Fill Ids')
-t.Start()
-
-for i in collector:
-	
-	print(i.Id.IntegerValue)
-	i.LookupParameter('ID Revit').Set(i.Id.IntegerValue)
-	
-t.Commit()
+    "A plus tard!"
