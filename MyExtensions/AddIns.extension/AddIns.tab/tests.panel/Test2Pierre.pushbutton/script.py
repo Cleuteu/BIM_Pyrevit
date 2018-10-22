cb82d6bce62c0b5a3e9ca14f1c6bd5ce0d437c7b
Pierre Bolot <pbolot@b-p.fr>
2018-10-22 18:28:30 +0200

    modif test 2

--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Test2Pierre.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Test2Pierre.pushbutton/script.py
@@ -13,22 +13,46 @@ options = __revit__.Application.Create.NewGeometryOptions()
 SEBoptions = SpatialElementBoundaryOptions()
 roomcalculator = SpatialElementGeometryCalculator(doc)
 
-def get_selected_elements(doc):
-    try:
-        # Revit 2016
-        return [doc.GetElement(id)
-                for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
-    except:
-        # old method
-        return list(__revit__.ActiveUIDocument.Selection.Elements)
+# def get_selected_elements(doc):
+#     try:
+#         # Revit 2016
+#         return [doc.GetElement(id)
+#                 for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+#     except:
+#         # old method
+#         return list(__revit__.ActiveUIDocument.Selection.Elements)
 	
-el = get_selected_elements(doc)[0]
-print(el.Id)
-print(el.GroupId)
+# el = get_selected_elements(doc)[0]
+# print(el.Id)
+# print(el.GroupId)
 
-t = Transaction(doc, 'Set Param')
-t.Start()
+# t = Transaction(doc, 'Set Param')
+# t.Start()
 
-el.LookupParameter('ECO_lot').Set("God no")
+# el.LookupParameter('ECO_lot').Set("God no")
 
-t.Commit()
+# t.Commit()
+
+
+
+cat_list = [BuiltInCategory.OST_PipeCurves, BuiltInCategory.OST_DuctCurves,\
+			BuiltInCategory.OST_CableTray]
+
+cat = BuiltInCategory.OST_CableTray 
+
+element_collector = FilteredElementCollector(doc)\
+	.OfCategory(cat)\
+	.WhereElementIsNotElementType()\
+	.ToElements()
+
+icollection = List[ElementId]()
+
+param = "Niveau de r"+"\xe9"+"f"+"\xe9"+"rence"
+
+for element in element_collector:
+	# print(element.LookupParameter(param).AsValueString()+" !")
+	if element.LookupParameter(param).AsValueString() == "RDC-OLD":
+		icollection.Add(element.Id)
+	
+uidoc.Selection.SetElementIds(icollection)
+
