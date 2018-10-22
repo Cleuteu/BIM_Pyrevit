6c22ac0bfe48d9f58257626385afecc412d8c161
Pierre Bolot <pbolot@b-p.fr>
2018-10-22 09:00:27 +0200

    tests a la con

---
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Test2Pierre.pushbutton/script.py
@@ -0,0 +1,34 @@
+
+import clr
+import math
+clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPIUI') 
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import *
+from System.Collections.Generic import List
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+options = __revit__.Application.Create.NewGeometryOptions()
+SEBoptions = SpatialElementBoundaryOptions()
+roomcalculator = SpatialElementGeometryCalculator(doc)
+
+def get_selected_elements(doc):
+    try:
+        # Revit 2016
+        return [doc.GetElement(id)
+                for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+    except:
+        # old method
+        return list(__revit__.ActiveUIDocument.Selection.Elements)
+	
+el = get_selected_elements(doc)[0]
+print(el.Id)
+print(el.GroupId)
+
+t = Transaction(doc, 'Set Param')
+t.Start()
+
+el.LookupParameter('ECO_lot').Set("God no")
+
+t.Commit()
