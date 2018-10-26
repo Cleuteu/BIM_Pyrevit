66db71d5fffb9eb4cbd6b8ba5b837607b3d2952f
Thomas Faguet <th.faguet@gmail.com>
2018-10-26 16:11:06 +0200

    new function added

--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
@@ -1,83 +1,77 @@
+# IMPORT 
 import clr
 import System
 import math
+import Autodesk
 clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI') 
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
+from pyrevit import DB, UI 
 from System.Collections.Generic import List
 from pyrevit import forms
 from rpw.ui.forms import TextInput
 from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox, TextBox, Separator, Button)
 
-
+# VARIABLES UTILES
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 options = __revit__.Application.Create.NewGeometryOptions()
 SEBoptions = SpatialElementBoundaryOptions()
 roomcalculator = SpatialElementGeometryCalculator(doc)
 
-forms.select_views(title='Select Views', button_name='Select', width=1000, multiple=True, filterfunc=None, doc=None)
+collector = Autodesk.Revit.DB.FilteredElementCollector(doc)
 
 
+# TEST RECUPERATION NIVEAUX
 # collector = FilteredElementCollector(doc)
-# siteElement = collector.OfClass(SiteLocation).ToElements()
-# siteElemMaq = UnwrapElement(siteElement)
-# latitude = []
-# longitude = []
-# placeNameTest = []
-# placeName = []
-# indice2 = 0
-# for i in range(len(siteElemMaq)):
-#   placeNameTest.append(siteElemMaq[i].PlaceName)
-#   if placeNameTest[i]:
-#     indice2 = i
-# for i in range(len(siteElemMaq)):
-#   if i == indice2:
-#     latitude.append(siteElemMaq[i].Latitude)
-#     longitude.append(siteElemMaq[i].Longitude)
-#     placeName.append(siteElemMaq[i].PlaceName)
-  
-# #SITE LOCATION LINK /////////////////////////////////////////////////////////////
+# levelElements = collector.OfClass(Level).ToElements()
+
+# levelElevation = []
+# for lev in levelElements:
+#   levelElevation.append(lev.ProjectElevation)
+# levelName = []
+# for lev in levelElements:
+#   levelName.append(lev.LookupParameter("Nom").AsString())
+
+
+
 # collector = Autodesk.Revit.DB.FilteredElementCollector(doc)
 # linkInstances = collector.OfClass(Autodesk.Revit.DB.RevitLinkInstance)
 # linkDoc = []
-# for i in linkInstances:
-#   linkDoc.append(i.GetLinkDocument())
+# for links in linkInstances:
+#   linkDoc.append(links.GetLinkDocument())
     
 # collector = FilteredElementCollector(linkDoc[0])
-# siteElementLink = collector.OfClass(SiteLocation).ToElements()
-# siteElemLink = UnwrapElement(siteElementLink)
-# latLink = []
-# longLink = []
-# placeNameTestLink = []
-# placeNameLink = []
+# levelElementsLink = collector.OfClass(Level).ToElements()
+
+# levelElevationLink = []
+# for lev in levelElementsLink:
+#   levelElevationLink.append(lev.ProjectElevation)
+# levelNameLink = []
+# for lev in levelElementsLink:
+#   levelNameLink.append(lev.LookupParameter("Nom").AsString())
+
+# print(levelNameLink)
+
+
+def take_links(doc=doc):
+  linkInstances = collector.OfClass(Autodesk.Revit.DB.RevitLinkInstance)
+  print linkInstances
+  linkDoc = []
+  for links in linkInstances:
+    linkDoc.append(links.GetLinkDocument())
+  return linkDoc
+
+def collector_link(doc=doc):
+  return FilteredElementCollector(linkDoc[0])
+
+def take_levels_link(doc=linkDoc):
+  levelElementsLink = collector.OfClass(Level).ToElements()
+  levelElevationLink = []
+  for lev in levelElementsLink:
+    levelElevationLink.append(lev.ProjectElevation)
+  levelNameLink = []
+  for lev in levelElementsLink:
+    levelNameLink.append(lev.LookupParameter("Nom").AsString())
   
-# for i in range(len(siteElemLink)):
-#   placeNameTestLink.append(siteElemLink[i].PlaceName)
-#   if placeNameTestLink[i]:
-#     indice2 = i
-# for i in range(len(siteElemLink)):
-#   if i == indice2:
-#     latLink.append(siteElemLink[i].Latitude)
-#     longLink.append(siteElemLink[i].Longitude)
-#     placeNameLink.append(siteElemLink[i].PlaceName)
-
-# #OPERATIONS SUR LINK ET MAQUETTE SITE LOCATION /////////////////////////////////////////////////////
-# pourcentSiteLocation = 0
-# textSiteLocation = ""
-# if abs(latitude[0] - latLink[0]) < 0.001:
-#   pourcentSiteLocation = pourcentSiteLocation + 0.33333333333333333
-#   if abs(longitude[0] - longLink[0]) < 0.001:
-#     pourcentSiteLocation = pourcentSiteLocation + 0.33333333333333333
-#     if placeName in placeNameLink :
-#       pourcentSiteLocation = pourcentSiteLocation + 0.33333333333333333
-#       textSiteLocation = "Les coordonnées du site partagé sont les mêmes que le QNP"
-#     else:
-#       textSiteLocation = "Les coordonnées sont OK mais le nom de l'emplacement du site partagé est différent du QNP"
-#   else: 
-#     textSiteLocation = "Les coordonnées du site partagé ne sont pas les mêmes que le QNP"
-# else :
-#   textSiteLocation = "Les coordonnées du site partagé ne sont pas les mêmes que le QNP"
-
-
