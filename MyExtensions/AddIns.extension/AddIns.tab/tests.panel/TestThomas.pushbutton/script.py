770da48666335dbb2df1563e876dc9802a139819
Thomas Faguet <th.faguet@gmail.com>
2018-10-31 17:53:48 +0100

    extract data from link

--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
@@ -4,73 +4,29 @@ import rpw
 from rpw import revit, db, ui, DB, UI
 from rpw.ui.forms import *
 
-levels_Cat = db.Collector(of_category='Levels', is_not_type=True)
-level_Elements = levels_Cat.get_elements()
+# MAQUETTE
+level_category = db.Collector(of_category='Levels', is_not_type=True)
+level_elements = level_category.get_elements()
+level_name = [levels.LookupParameter("Nom").AsString() for levels in level_elements]
+level_elevation = [levels.ProjectElevation for levels in level_elements]
+
+base_point_category = db.Collector(of_category='OST_ProjectBasePoint')
+base_point_elements = base_point_category.get_elements()
+base_point_elevation = [pdb.LookupParameter("Elév.").AsValueString() for pdb in base_point_elements]
+
+# LINK
+collector_maq = DB.FilteredElementCollector(revit.doc)
+linkInstances = collector_maq.OfClass(DB.RevitLinkInstance)
+linkDoc = [links.GetLinkDocument() for links in linkInstances]
+collector_link = DB.FilteredElementCollector(linkDoc[0])
+
+level_elements_link = collector_link.OfClass(DB.Level).ToElements()
+level_name_link = [levels.LookupParameter("Nom").AsString() for levels in level_elements_link]
+level_elevation_link = [levels.ProjectElevation for levels in level_elements_link]
+
+print base_point_elevation
+
+# OUT
 
 
-
-
-
-
-# VARIABLES UTILES
-# doc = __revit__.ActiveUIDocument.Document
-# uidoc = __revit__.ActiveUIDocument
-# options = __revit__.Application.Create.NewGeometryOptions()
-# SEBoptions = SpatialElementBoundaryOptions()
-# roomcalculator = SpatialElementGeometryCalculator(doc)
-
-# collector = Autodesk.Revit.DB.FilteredElementCollector(doc)
-
-
-# TEST RECUPERATION NIVEAUX
-# collector = FilteredElementCollector(doc)
-# levelElements = collector.OfClass(Level).ToElements()
-
-# levelElevation = []
-# for lev in levelElements:
-#   levelElevation.append(lev.ProjectElevation)
-# levelName = []
-# for lev in levelElements:
-#   levelName.append(lev.LookupParameter("Nom").AsString())
-
-
-
-# collector = Autodesk.Revit.DB.FilteredElementCollector(doc)
-# linkInstances = collector.OfClass(Autodesk.Revit.DB.RevitLinkInstance)
-# linkDoc = []
-# for links in linkInstances:
-#   linkDoc.append(links.GetLinkDocument())
-    
-# collector = FilteredElementCollector(linkDoc[0])
-# levelElementsLink = collector.OfClass(Level).ToElements()
-
-# levelElevationLink = []
-# for lev in levelElementsLink:
-#   levelElevationLink.append(lev.ProjectElevation)
-# levelNameLink = []
-# for lev in levelElementsLink:
-#   levelNameLink.append(lev.LookupParameter("Nom").AsString())
-
-# print(levelNameLink)
-
-
-# def take_links(doc=doc):
-#   linkInstances = collector.OfClass(Autodesk.Revit.DB.RevitLinkInstance)
-#   print linkInstances
-#   linkDoc = []
-#   for links in linkInstances:
-#     linkDoc.append(links.GetLinkDocument())
-#   return linkDoc
-
-# def collector_link(doc=doc):
-#   return FilteredElementCollector(linkDoc[0])
-
-# def take_levels_link(doc=linkDoc):
-#   levelElementsLink = collector.OfClass(Level).ToElements()
-#   levelElevationLink = []
-#   for lev in levelElementsLink:
-#     levelElevationLink.append(lev.ProjectElevation)
-#   levelNameLink = []
-#   for lev in levelElementsLink:
-#     levelNameLink.append(lev.LookupParameter("Nom").AsString())
   
