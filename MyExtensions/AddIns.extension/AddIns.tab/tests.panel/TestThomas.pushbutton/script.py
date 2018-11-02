8f0f05c89561f634658fb3ddf994917151c35a35
Thomas Faguet <th.faguet@gmail.com>
2018-11-02 17:00:57 +0100

    create levels and grids from link

--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
@@ -1,20 +1,21 @@
 # IMPORT 
-
 import rpw
 from rpw import revit, db, ui, DB, UI
 from rpw.ui.forms import *
+
+
 
 # MAQUETTE
 level_category = db.Collector(of_category='Levels', is_not_type=True)
 level_elements = level_category.get_elements()
 level_name = [levels.LookupParameter("Nom").AsString() for levels in level_elements]
-level_elevation = [levels.ProjectElevation for levels in level_elements]
+level_elevation = [levels.Elevation for levels in level_elements]
+level_id = [levels.Id for levels in level_elements]
 
-base_point_category = db.Collector(of_category='OST_ProjectBasePoint')
-base_point_elements = base_point_category.get_elements()
-base_point_elevation = [pdb.LookupParameter("Elév.").AsValueString() for pdb in base_point_elements]
+grid_category = db.Collector(of_category='OST_Grids', is_not_type=True)
+grid_elements = grid_category.get_elements()
 
-# LINK
+# COLLECT LEVELS LINK
 collector_maq = DB.FilteredElementCollector(revit.doc)
 linkInstances = collector_maq.OfClass(DB.RevitLinkInstance)
 linkDoc = [links.GetLinkDocument() for links in linkInstances]
@@ -22,11 +23,45 @@ collector_link = DB.FilteredElementCollector(linkDoc[0])
 
 level_elements_link = collector_link.OfClass(DB.Level).ToElements()
 level_name_link = [levels.LookupParameter("Nom").AsString() for levels in level_elements_link]
-level_elevation_link = [levels.ProjectElevation for levels in level_elements_link]
+level_elevation_link = [levels.Elevation for levels in level_elements_link]
 
-print base_point_elevation
+# COLLECT GRIDS LINK
+collector_maq = DB.FilteredElementCollector(revit.doc)
+linkInstances = collector_maq.OfClass(DB.RevitLinkInstance)
+linkDoc = [links.GetLinkDocument() for links in linkInstances]   
+collector_link = DB.FilteredElementCollector(linkDoc[0])
+
+grid_elements_link = collector_link.OfClass(DB.Grid).ToElements()
+grid_name_link = [grids.Name for grids in grid_elements_link]
+grid_length_link = [grids.Curve.Length for grids in grid_elements_link]
+grid_origin_link = [grids.Curve.Origin for grids in grid_elements_link]
+grid_direction_link = [grids.Curve.Direction for grids in grid_elements_link]
+
+# CREATE LEVELS
+# with db.Transaction('create levels'):
+#   for k in range(len(level_elements_link)):
+#     NewLevel = DB.Level.Create(document = revit.doc, elevation = 0)
+#     NewLevel.Name = level_name_link[k]
+#     NewLevel.Elevation = level_elevation_link[k]
+
+# DELETE LEVELS 
+# with db.Transaction('delete levels'):
+#   for levels in level_id:
+#     DB.Document.Delete(revit.doc, levels)
+
+# CREATE LEVELS
+# with db.Transaction('create grids'):
+#   for k in range(len(grid_elements_link)):
+    # start = db.XYZ(0,0,0)
+    # end = db.XYZ(0,0,0)
+    # BaseLine = db.Line.new([0,0],[1,1])
+    # NewGrid = DB.Grid.Create(document = revit.doc, line = BaseLine)
+    # NewGrid.Name = grid_name_link[k]
+    # NewGrid.Curve.Origin = grid_origin_link[k]
+    # NewGrid.Curve.Length = grid_length_link[k]
+    # NewGrid.Curve.Direction = grid_direction_link[k]
 
 # OUT
-
+# print grid_origin_link, grid_length_link, grid_direction_link
 
   
