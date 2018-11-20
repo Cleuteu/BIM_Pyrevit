a3093aa666ed2a6efc3d83f4d35a5aea186d1d51
Thomas Faguet <th.faguet@gmail.com>
2018-11-20 18:14:40 +0100

    insert link from folders

--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestThomas.pushbutton/script.py
@@ -4,6 +4,18 @@ from rpw import revit, db, ui, DB, UI
 from rpw.ui.forms import *
 
 
+# INSERT LINK
+#User select a revit file in folders
+filepath = select_file('Revit Model (*.rvt)|*.rvt')
+#start transaction
+with db.Transaction('insert link'):
+  for path in filepath:
+    linkpath = DB.ModelPathUtils.ConvertUserVisiblePathToModelPath(filepath)   
+  linkoptions = DB.RevitLinkOptions(relative=True)
+  linkloadresult = DB.RevitLinkType.Create(revit.doc, linkpath, linkoptions)
+  DB.RevitLinkInstance.Create(revit.doc, linkloadresult.ElementId)
+
+
 
 # MAQUETTE
 level_category = db.Collector(of_category='Levels', is_not_type=True)
@@ -44,32 +56,35 @@ linkDoc = [links.GetLinkDocument() for links in linkInstances]
 collector_link = DB.FilteredElementCollector(linkDoc[0])
 coord = DB.Document.AcquireCoordinates(linkInstances)
 
-# CREATE LEVELS
+# # CREATE LEVELS
 # with db.Transaction('create levels'):
 #   for k in range(len(level_elements_link)):
 #     NewLevel = DB.Level.Create(document = revit.doc, elevation = 0)
 #     NewLevel.Name = level_name_link[k]
 #     NewLevel.Elevation = level_elevation_link[k]
 
-# DELETE LEVELS 
+# # DELETE LEVELS 
 # with db.Transaction('delete levels'):
 #   for levels in level_id:
 #     DB.Document.Delete(revit.doc, levels)
 
-# CREATE LEVELS
+# # CREATE GRIDS
 # with db.Transaction('create grids'):
 #   for k in range(len(grid_elements_link)):
-    # start = db.XYZ(0,0,0)
-    # end = db.XYZ(0,0,0)
-    # BaseLine = db.Line.new([0,0],[1,1])
-    # NewGrid = DB.Grid.Create(document = revit.doc, line = BaseLine)
-    # NewGrid.Name = grid_name_link[k]
-    # NewGrid.Curve.Origin = grid_origin_link[k]
-    # NewGrid.Curve.Length = grid_length_link[k]
-    # NewGrid.Curve.Direction = grid_direction_link[k]
+#     start = db.XYZ(0,0,0)
+#     end = db.XYZ(0,0,0)
+#     BaseLine = db.Line.new([0,0],[1,1])
+#     NewGrid = DB.Grid.Create(document = revit.doc, line = BaseLine)
+#     NewGrid.Name = grid_name_link[k]
+#     NewGrid.Curve.Origin = grid_origin_link[k]
+#     NewGrid.Curve.Length = grid_length_link[k]
+#     NewGrid.Curve.Direction = grid_direction_link[k]
+
+
+
 
 # OUT
-print coord
-# print grid_origin_link, grid_length_link, grid_direction_link
+
+print grid_origin_link, grid_length_link, grid_direction_link
 
   
