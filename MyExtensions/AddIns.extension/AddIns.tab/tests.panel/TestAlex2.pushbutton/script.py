3a89fb502de1772d3c57e656e6b67de2395a20ad
Cleuteu <>
2019-01-21 15:45:41 +0100

    added HSP

--- MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestAlex2.pushbutton/script.py
+++ MyExtensions/AddIns.extension/AddIns.tab/tests.panel/TestAlex2.pushbutton/script.py
@@ -1,208 +1,113 @@
+"""Etiquetage de poutres CANOPY!"""
 
-# # Get SharedParameters in the doc and their categories
-# iterator = doc.ParameterBindings.ForwardIterator()
-# elems = []
-# guids = []
-# isinst = []
-# cats = {}
-# while iterator.MoveNext():
-#     thesecats = []
-#     for cat in iterator.Current.Categories:
-#       try:
-#         pCatFrench = Revit.Elements.Category.ById(cat.Id.IntegerValue).Name
-#         if pCatFrench == "Murs":
-#           pCatEnglish = "Wall"
-#         elif pCatFrench == "Sols":
-#           pCatEnglish = "Slab"
-#         elif pCatFrench == "Pi\xe8ces":
-#           pCatEnglish = "Room"
-#         elif pCatFrench == "Portes":
-#           pCatEnglish = "Doors"
-#         elif pCatFrench == "Poteaux porteurs":
-#           pCatEnglish = "Column"
-#         # TODO : Traduire toutes les cat vers IFC
-#         thesecats.append(pCatEnglish)
-#       except:
-#           thesecats.append(None)
-#   cats[iterator.Key.Name] = thesecats
-# print cats
+# -*- coding: utf-8 -*-
+e_a = str("\xe9")
+a_a = str("\xe0")
 
+__title__ = 'Beam tagging\nCANOPY'
 
+__doc__ = 'Ce programme remplit les arases inferieures mini et maxi des poutres '\
+      '(parametres AI_Min et AI_Max) de la vue active du projet CANOPY'
 
+# from pyrevit import revit, DB, UI
+# from pyrevit import script
+# from pyrevit import forms
 
-#Sauvegarde du Pyrevit Economiste Excel
 import clr
-import System
 import math
-clr.AddReference('RevitAPI')
+from pyrevit import forms
+clr.AddReference('RevitAPI') 
 clr.AddReference('RevitAPIUI') 
 from Autodesk.Revit.DB import *
-from Autodesk.Revit.UI import *
-from System.Collections.Generic import List
-from pyrevit import forms
-from rpw.ui.forms import (Alert, TextInput, FlexForm, Label, ComboBox, TextBox, TextBox, Separator, Button)
-
+from Autodesk.Revit.UI import * 
 
 doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
+
 options = __revit__.Application.Create.NewGeometryOptions()
-SEBoptions = SpatialElementBoundaryOptions()
-roomcalculator = SpatialElementGeometryCalculator(doc)
 
-td_button = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No
+BP_collector = FilteredElementCollector(doc)\
+          .OfCategory(BuiltInCategory.OST_ProjectBasePoint)\
+          .WhereElementIsNotElementType()\
+          .ToElements()
 
-res = TaskDialog.Show("Importation from Excel","Attention :\n- Les ids des elements doivent etre en colonne 1\n- Les noms exacts (avec majuscules) des parametres partages doivent etre en ligne 1\n- Aucun accent ou caractere special dans le fichier Excel", td_button)
+beam_collector = FilteredElementCollector(doc,doc.ActiveView.Id)\
+          .OfCategory(BuiltInCategory.OST_StructuralFraming)\
+          .WhereElementIsNotElementType()\
+          .ToElements()
+      
+    
+td_button = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.Cancel | TaskDialogCommonButtons.No
+
+res = TaskDialog.Show("Etiquetage de poutres","Voulez-vous lancer l'"+e_a+"tiquetage des poutres dans la vue active?",td_button)
 
 if res == TaskDialogResult.Yes:
-               
-               # Ungroup function 
-    def Ungroup(group):
-                  group.UngroupMembers()
-                  
-   # Regroup function 
-    def Regroup(groupname,groupmember):
-                  newgroup = doc.Create.NewGroup(groupmember)
-                  newgroup.GroupType.Name = str(groupname)
-   
-    def convertStr(s):
-       """Convert string to either int or float."""
-       try:
-           ret = int(s)
-       except ValueError:
-           ret = 0
-       return ret
-                  
-    t = Transaction(doc, 'Read Excel spreadsheet.') 
-    t.Start()
-   
-    #Accessing the Excel applications.
-    xlApp = System.Runtime.InteropServices.Marshal.GetActiveObject('Excel.Application')
-    count = 1
 
-    dicWs = {}
-    count = 1
-    for i in xlApp.Worksheets:
-        dicWs[i.Name] = i
-        count += 1
-
-    components = [Label('Pick a category:'),
-              ComboBox('combobox2', {'Doors': 0, 'Rooms': 1}),
-              Label('Select the name of Excel sheet to import:'),
-              ComboBox('combobox', dicWs),
-              Label('Enter the number of rows in Excel you want to integrate to Revit:'),
-              TextBox('textbox', Text="60"),
-              Label('Enter the number of colones in Excel you want to integrate to Revit:'),
-              TextBox('textbox2', Text="2"),
-              Separator(),
-              Button('OK')]
-    form = FlexForm('Title', components)
-    form.show()
-
-    worksheet = form.values['combobox']
-    rowEnd = convertStr(form.values['textbox'])
-    colEnd = convertStr(form.values['textbox2'])
-    category = form.values['combobox2']
-
-    if category == 0:
-      collector = FilteredElementCollector(doc)\
-            .OfCategory(BuiltInCategory.OST_Doors)\
-              .WhereElementIsNotElementType()\
-              .ToElements()
-    else:
-      collector = FilteredElementCollector(doc)\
-            .OfCategory(BuiltInCategory.OST_Rooms)\
-              .WhereElementIsNotElementType()\
-              .ToElements()
-
-    #Row, and Column parameters
-    rowStart = 1
-    column_id = 1
-    colStart = 2
-
-    #Get parameters in the model
-    params_element_set = collector[0].Parameters
-    params_element_name = []
-    for param_element in params_element_set:
-     params_element_name.append(param_element.Definition.Name)
-
-    # Using a loop to read a range of values and print them to the console.
-    array = []
-    param_names_excel = []
-    data = {}
-    for r in range(rowStart, rowEnd):
-        data_id = worksheet.Cells(r, column_id).Text
-        data_id_int = convertStr(data_id)
-        if data_id_int != 0:
-            data = {'id': data_id_int}
-            for c in range(colStart, colEnd):
-                data_param_value = worksheet.Cells(r, c).Text
-                data_param_name = worksheet.Cells(1, c).Text
-                if data_param_name != '':
-                    param_names_excel.append(data_param_name)
-                    if data_param_value != '':
-                        data[data_param_name] = data_param_value
-            array.append(data)
-                                         
-    t.Commit()
-
-    #Check if parameters from Excel are in the model
-    missing_param = []
-    for param in param_names_excel:
-      if (param not in params_element_name) and param not in (missing_param):
-        missing_param.append(param)
+  for BP in BP_collector:
+    zBP = BP.get_Parameter(BuiltInParameter.BASEPOINT_ELEVATION_PARAM).AsDouble()/3.2808399
     
-    #Ask to continue if some parameters are missing
-    nbr_missing_param = len(missing_param)          
-    if nbr_missing_param != 0:
-      miss_button = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No
-      miss = TaskDialog.Show("Parameters missing", "Some parameters in Excel document are missing in Revit : " + ", ".join(missing_param) + ".\nThe plugin cannot write in this parameters.\nDo you want to continue?" , miss_button)
-      if miss == TaskDialogResult.Yes:
+  tg = TransactionGroup(doc, 'Tag all beams in active view')
 
-        unfoundelements = []
-       
-        t = Transaction(doc, 'Feed elements')
-        t.Start()
+  tg.Start()
 
-        i = 0
-        for elementhash in array:
-            idInt = int(elementhash['id'])
-            i += 1
-            try :
-                element_id = ElementId(idInt)
-                element = doc.GetElement(element_id)
-                groupId = element.GroupId
-                if str(groupId) != "-1":
-                    group = doc.GetElement(groupId)
-                    groupname = group.Name
-                    groupmember = group.GetMemberIds()
-                    Ungroup(group)
-                               
-                for param in param_names_excel:
-                    if (param in params_element_name) and (param in elementhash):
-                        element.LookupParameter(param).Set(elementhash[param])
-                                              
-                if str(groupId) != "-1":
-                    Regroup(groupname,groupmember)
-                        # if "(membre exclu)" in group.GroupType.Name:
-                        # group.GroupType.Name = groupname
-                print("element " + str(idInt) + " : OK - Avancement :" + str(i) + "/" + str(rowEnd))
+  for beam in beam_collector:
+    # if beam.Id not in TaggegBeamsList:
+      try:
+        print("Nom de la poutre : " + beam.Name)
+        print("ID : " + str(beam.Id))
+        print("Niveau de r"+e_a+"f"+e_a+"rence : " + str(beam.LookupParameter('Niveau de r'+e_a+'f'+e_a+'rence').AsValueString()))
+        start = beam.Location.Curve.GetEndPoint(0)
+        print(start)
+        end = beam.Location.Curve.GetEndPoint(1)
+        print(end)
+        z_min = zBP + ((beam.get_Geometry(options).GetBoundingBox().Min).Z)/3.2808399
+        print("AI_Inf : " + str(z_min) + "m")
+        
+        # Si la poutre est en pente
+        if beam.LookupParameter('El'+e_a+'vation '+a_a+' la base').AsDouble() == 0:
+          print("EN PENTE")
+          t = Transaction(doc, 'Tag beam')
+          t.Start()
+          try:
+            delta = abs(float(beam.LookupParameter("D"+e_a+"calage du niveau de d"+e_a+"part").AsValueString())-float(beam.LookupParameter("D"+e_a+"calage du niveau d'arriv"+e_a+"e").AsValueString()))\
+                  +abs(float(beam.LookupParameter("Valeur de d"+e_a+"calage de l'extr"+e_a+"mit"+e_a+" Z").AsValueString())-float(beam.LookupParameter("Valeur de d"+e_a+"calage Z de d"+e_a+"but").AsValueString()))
+          except:
+            delta = abs(float(beam.LookupParameter("D"+e_a+"calage du niveau de d"+e_a+"part").AsValueString())-float(beam.LookupParameter("D"+e_a+"calage du niveau d'arriv"+e_a+"e").AsValueString()))
+          z_max = z_min + delta
+          print("AI_Max : " + str(z_max) + "m")
+          beam.LookupParameter('AI_Min').Set(" ")
+          beam.LookupParameter('AI_Min').Set(str(round(z_min,2)))
+          beam.LookupParameter('AI_Max').Set(" ")
+          beam.LookupParameter('AI_Max').Set(str(round(z_max,2)))
+          cen=XYZ((start.X+end.X)/2,(start.Y+end.Y)/2,(z_min+z_max)/2)
+          print(cen)
+          print("\n")
+          beam_tag = doc.Create.NewTag(doc.ActiveView,beam,False,TagMode.TM_ADDBY_CATEGORY,TagOrientation.Horizontal,cen)
+          t.Commit()
+        # Si la poutre est horizontale
+        else :
+          print("HORIZONTALE")
+          t = Transaction(doc, 'Tag beam')
+          t.Start()
+          beam.LookupParameter('AI_Min').Set(" ")
+          beam.LookupParameter('AI_Min').Set(str(round(z_min,2)))
+          beam.LookupParameter('AI_Max').Set(" ")
+          cen=XYZ((start.X+end.X)/2,(start.Y+end.Y)/2,z_min)
+          print(cen)
+          print("\n")
+          beam_tag = doc.Create.NewTag(doc.ActiveView,beam,False,TagMode.TM_ADDBY_CATEGORY,TagOrientation.Horizontal,cen)
+          t.Commit()
+      
+      except:
+        print(" ")
+        
+  tg.Assimilate()
+  
+  td_button2 = TaskDialogCommonButtons.Ok
 
-            except:
-                print(str(idInt) + " not in REVIT doc")
-                unfoundelements.append(idInt)
-                                    
-        print("Job done!")
-       
-        t.Commit()
-
-        if len(unfoundelements) != 0:
-            print(str(len(unfoundelements)) + " elements not found : ")
-            print(unfoundelements)
-
-        if nbr_missing_param != 0:
-          print(str(len(missing_param)) + " parameters from Excel not found in Revit (wrong spelling in Excel or just not in this Revit document?) : ")
-          print(missing_param)
-      else:
-        Alert('Come back when your changes are done', title = "End of the program", exit = True)
+  res2 = TaskDialog.Show("Mise au propre","Veuillez checker la non superposition des "+e_a+"tiquettes pour un r"+e_a+"sultat plus lisible!",td_button2)
+        
 else:
-  Alert('A plus tard!', title = "End of the program", exit = True)
+  print("Une autre fois peut-etre...")
+  
+
+
